2f257c7c7c8221cd07861329316242ff699e3902
m4cd <maciej.ziebicki@outlook.com>
2023-06-13 14:57:47 +0200

    metrics implemented

--- webServer.go
+++ webServer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -17,13 +18,45 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
+func customHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) customMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	hits := "Hits: " + fmt.Sprint(cfg.fileserverHits)
+	w.Write([]byte(hits))
+
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	serverRoot := "."
 	serverPort := "8080"
 
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(serverRoot)))
+	//mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(serverRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(serverRoot)))))
+	mux.HandleFunc("/healthz", customHandler)
+	mux.HandleFunc("/metrics", apiCfg.customMetricsHandler)
 
 	corsMux := middlewareCors(mux)
 
